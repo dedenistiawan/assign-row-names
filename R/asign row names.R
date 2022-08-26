e82bba2e5d03dc3a3bca4f64453dda0b1ef80b33
dedenistiawan <dedenistiawan@gmail.com>
2022-08-26 11:36:32 +0700

    commit

--- R/asign row names.R
+++ R/asign row names.R
@@ -2,8 +2,8 @@ library(readr)
 urlfile = "https://raw.githubusercontent.com/dedenistiawan/Dataset/main/BDT.csv"
 Data<-read.csv(url(urlfile), row.names = "Kabupaten")
 
-#data<-read.csv("D:/My Drive/deden-research/Dataset/BDT.csv",row.names = "Kabupaten")
-
+######data<-read.csv("D:/My Drive/deden-research/Dataset/BDT.csv",row.names = "Kabupaten")
+#cara
 library(ggplot2)
 library(factoextra)
 distance <- get_dist(Data)
